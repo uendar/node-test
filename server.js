a851de1c8bf494a6e2f86c34307ba6552d8cee7e
EndarUliu <endar8@gmail.com>
2018-09-14 15:07:41 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/web-server/public/'));
+
+app.use((req, res, next)=>{
+   var now = new Date().toString();
+   var log = `${now} : ${req.method} ${req.url}`;
+  // console.log();
+    fs.appendFile('server.log', log+ '\n', (err)=>{
+        if(err){
+            console.log("Unable to append to server.log");
+        }
+    })
+  next();
+  
+});
+
+// app.use((req, res, next) =>{
+//     res.render('maintenance.hbs');
+// })
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+   return new Date().getFullYear();
+});
+
+hbs.registerHelper('getData', (text)=>{
+    return text
+});
+
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs',{
+        pageTitle:'Home Page',
+        info:'Hey welcom to our website.. It is under construction for now....'
+     });
+});
+
+
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',  
+        info:'Ths is just for about page...'
+    })
+});
+
+
+app.get('/bad',(req, res)=>{
+    res.statusCode =404;
+    res.send({
+      error:'A error occured with your request!'
+    });
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+});
+
